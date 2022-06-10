11ddd06a7c7cd52675bbaab2f36f63deedff5d63
Sanket-Nagare <107258818+Sanket-Nagare@users.noreply.github.com>
2022-06-10 21:08:28 +0530

    Add files via upload

---
+++ Firstprogram.java
@@ -0,0 +1,10 @@
+class Firstprogram{
+public static void main(String[] args){
+String Book="BlackFriday";
+String Author="Ravi Belegare";
+float price=206.10f;
+System.out.println(Book);
+System.out.println(Author);
+System.out.println(price);
+}
+}
